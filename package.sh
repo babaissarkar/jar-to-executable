5608ff905f27efcd80fd497f5dc5bc00cca868d3
Subhraman Sarkar <suvrax@gmail.com>
2025-10-31 20:09:42 +0530

    desktop-file input, custom icon/desktop-file, download AppRun

--- package.sh
+++ package.sh
@@ -16,8 +16,11 @@ PROGRAM_NAME=${PROGRAM_NAME:-$PROGRAM}
 JAR_DIR=${JAR_DIR:-jar}
 INPUT=${INPUT:-${JAR_DIR}/${PROGRAM}.jar}
 OUTPUT=${OUTPUT:-${PROGRAM_NAME}.AppImage}
+PROGRAM_ICON=${PROGRAM_ICON:-${PROGRAM}.png}
+PROGRAM_DESKTOP_FILE=${PROGRAM_DESKTOP_FILE:-${PROGRAM}.desktop}
 MODULES=$(jdeps --ignore-missing-deps --multi-release 17 --print-module-deps "${INPUT}")
 APPIMAGE_DOWNLOAD_URL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+APPRUN_DOWNLOAD_URL="https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-x86_64"
 APPIMAGETOOL="/tmp/appimagetool-x86_64.AppImage"
 
 run_jlink() {
@@ -33,13 +36,16 @@ run_jlink() {
 # Run jlink to create runtime
 run_jlink
 
+# download AppRun
+wget -nc --verbose -O AppRun ${APPRUN_DOWNLOAD_URL}
+
 # create directory structure
 mkdir -p ${PROGRAM_NAME}.AppDir/usr/share/${PROGRAM}
-cp ${INPUT} "${PROGRAM_NAME}.AppDir/usr/share/${PROGRAM}/"
+cp ${INPUT} ${PROGRAM_NAME}.AppDir/usr/share/${PROGRAM}/
 cp ${PROGRAM} ${PROGRAM_NAME}.AppDir/usr/bin/
 cp AppRun ${PROGRAM_NAME}.AppDir/
-cp ${PROGRAM}.png ${PROGRAM_NAME}.AppDir/
-cp ${PROGRAM}.desktop ${PROGRAM_NAME}.AppDir/
+cp ${PROGRAM_ICON} ${PROGRAM_NAME}.AppDir/
+cp ${PROGRAM_DESKTOP_FILE} ${PROGRAM_NAME}.AppDir/
 
 # download appimagetool
 pwd && ls -lh
@@ -51,3 +57,5 @@ ${APPIMAGETOOL} --appimage-extract
 
 # run appimagetool
 ARCH=x86_64 squashfs-root/AppRun "${PROGRAM_NAME}.AppDir" ${OUTPUT}
+
+echo "AppImage created successfully! Path: ${OUTPUT}"
